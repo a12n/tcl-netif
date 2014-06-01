b172933fbf67c9523c7430dd829994d4e4a0e4b8
Anton Yabchinskiy <arn@bestmx.ru>
2014-06-01 10:08:22 +0400

    netif.tcl: source OS specific script

--- netif.tcl
+++ netif.tcl
@@ -1,1 +1,9 @@
 package provide netif 0.0.1
+
+proc _ssplit {str sep} {
+    return [split [string map [list $sep \uFFFF] $str] \uFFFF]
+}
+
+set os [string tolower [lindex $tcl_platform(os) 0]]
+set dir [file dirname [info script]]
+source [file join $dir netif-$os.tcl]

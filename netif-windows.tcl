a0bd525addd03e9f57ac08f3c673815f8fcb78cb
Anton Yabchinskiy <arn@bestmx.ru>
2014-06-02 22:59:22 +0400

    netif-windows.tcl: ifname -> ifnam, for consistency with -linux.tcl

--- netif-windows.tcl
+++ netif-windows.tcl
@@ -1,15 +1,15 @@
 proc netif {{name {}}} {
     set ipconfig [exec ipconfig /all]
-    set ifname_pat {^([^:]*(?:Ethernet|PPP)[^:]*):?$}
+    set ifnam_pat {^([^:]*(?:Ethernet|PPP)[^:]*):?$}
     set ifaddr_pat {^\s*(?:IPv4|IP)[ -].*:\s*(\S+)}
     set ans {}
     foreach line [split $ipconfig \n] {
-        if {[regexp -line $ifname_pat $line _ ifname]} {
+        if {[regexp -line $ifnam_pat $line _ ifnam]} {
             # ok
         } elseif {[regexp -line $ifaddr_pat $line _ ifaddr]} {
-            if {[info exists ifname]} {
-                lappend ans [list $ifname $ifaddr]
-                unset ifaddr ifname
+            if {[info exists ifnam]} {
+                lappend ans [list $ifnam $ifaddr]
+                unset ifaddr ifnam
             }
         }
     }

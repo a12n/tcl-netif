b2141252c133aaac82d69ccfba2c199df6708f64
Anton Yabchinskiy <arn@bestmx.ru>
2014-06-03 12:19:14 +0400

    netif-windows.tcl: return only connected interfaces

--- netif-windows.tcl
+++ netif-windows.tcl
@@ -13,5 +13,15 @@ proc netif {} {
             }
         }
     }
+    # Remove non-connected interfaces
+    set netsh [exec netsh interface ipv4 show interface]
+    set state_pat {^\s*\d+\s+\d+\s+\d+\s+(\S+)\s+(.+)$}
+    foreach line [split $netsh \n] {
+        if {[regexp -line $state_pat $line _ state ifnam]} {
+            if {$state ne {connected}} {
+                set ans [dict remove $ans $ifnam]
+            }
+        }
+    }
     return $ans
 }

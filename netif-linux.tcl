7f5c3fb804bb72b84075b276a10ab86fb03c0094
Anton Yabchinskiy <arn@bestmx.ru>
2014-10-20 12:07:35 +0400

    netif-linux: return MTU

--- netif-linux.tcl
+++ netif-linux.tcl
@@ -4,9 +4,10 @@ proc netif {} {
     set ifstrs [_ssplit $ifconfig \n\n]
     set ans [dict create]
     foreach ifstr $ifstrs {
-        set pattern {^(\S+).*inet addr:\s*(\S+)}
-        if {[regexp $pattern $ifstr _ ifnam ifaddr]} {
-            dict set ans $ifnam $ifaddr
+        set pattern {^(\S+).*inet addr:\s*(\S+).*MTU:\s*(\d+)}
+        if {[regexp $pattern $ifstr _ ifnam ifaddr mtu]} {
+            dict set ans $ifnam addrs $ifaddr
+            dict set ans $ifnam mtu $mtu
         } else {
             error {Device has no address}
         }

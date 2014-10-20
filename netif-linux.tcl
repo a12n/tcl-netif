9d3c1e5c9d8a68a679a7fa7b34da9d461ebbb583
Anton Yabchinskiy <arn@bestmx.ru>
2014-10-20 12:20:42 +0400

    netif-linux: return {R,T}X counters

--- netif-linux.tcl
+++ netif-linux.tcl
@@ -4,12 +4,14 @@ proc netif {} {
     set ifstrs [_ssplit $ifconfig \n\n]
     set ans [dict create]
     foreach ifstr $ifstrs {
-        set pattern {^(\S+).*inet addr:\s*(\S+).*MTU:\s*(\d+)}
-        if {[regexp $pattern $ifstr _ ifnam ifaddr mtu]} {
+        set pattern {^(\S+).*inet addr:\s*(\S+).*MTU:\s*(\d+).*RX bytes:\s*(\d+).*TX bytes:\s*(\d+)}
+        if {[regexp $pattern $ifstr _ ifnam ifaddr mtu rx tx]} {
             dict set ans $ifnam addrs $ifaddr
             dict set ans $ifnam mtu $mtu
+            dict set ans $ifnam rx $rx
+            dict set ans $ifnam tx $tx
         } else {
-            error {Device has no address}
+            error {Device is not configured}
         }
     }
     return $ans
